ff2d168af9d440bdd111acc21a0e73685197a140
luoxi <2652445066@qq.com>
2022-01-04 22:56:32 +0800

    实现 运行微队列函数 把then函数中的参数函数加入到形成的队列中

--- my-promise/Mypromise.js
+++ my-promise/Mypromise.js
@@ -1,6 +1,6 @@
 /*
  * @Author: luoxi
- * @LastEditTime: 2022-01-02 22:41:25
+ * @LastEditTime: 2022-01-04 22:53:52
  * @LastEditors: your name
  * @Description: 手写promise A+规范
  */
@@ -8,6 +8,28 @@
 const PENDING = "pending";
 const FULFILLED = "fulfilled";
 const REJECTED = "rejected";
+/**
+ * @description: 运行一个微队列任务 把传递的函数放到微队列中
+ * @param {*Function} callback
+ * @return {*}
+ */
+function runMicroTask(callback) {
+  // 判断环境为node 环境
+  if (process && process.nextTick) {
+    process.nextTick(callback);
+  } else if (MutationObserver) {
+    //浏览器环境
+    const p = document.createElement("p");
+    const observer = new MutationObserver(callback);
+    observer.observe(p, {
+      childList: true, // 观察改元素内部的变化
+    });
+    p.innerHTML = "1";
+  } else {
+    //其他环境
+    setTimeout(callback, 0);
+  }
+}
 class MyPromise {
   /**
    * @description: 创建一个Promise
@@ -17,12 +39,40 @@ class MyPromise {
   constructor(executor) {
     this._state = PENDING; //状态
     this._value = undefined; //数据
+    this._handler = []; //处理函数形成的队列
     try {
-        executor(this._rosolve.bind(this), this._reject.bind(this));
+      executor(this._rosolve.bind(this), this._reject.bind(this));
     } catch (error) {
-        this._reject(error)
-        
+      this._reject(error);
     }
+  }
+  /**
+   * @description: 向处理队列中添加一个函数
+   * @param {* Function} executor  添加的函数
+   * @param {* String} state 该函数什么状态下执行
+   * @param {* Function} resolve 让then函数返回的Promise成功
+   * @param {* Function} reject 让then函数返回的Promise失败
+   * @return {*}
+   */
+  _pushHandler(executor, state, resolve, reject) {
+    this._handler.push({
+      executor,
+      state,
+      resolve,
+      reject,
+    });
+  }
+  /**
+   * @description: PromiseA+ 规范里的then
+   * @param {*Function} onFulfilled
+   * @param {*Function} onRejected
+   * @return {*}
+   */
+  then(onFulfilled, onRejected) {
+    return new MyPromise((resolve, reject) => {
+      this._pushHandler(onFulfilled, FULFILLED, resolve, reject);
+      this._pushHandler(onRejected, REJECTED, resolve, reject);
+    });
   }
   /**
    * @description:
@@ -31,10 +81,9 @@ class MyPromise {
    * @return {*}
    */
   _changeState(newState, value) {
-    if(this._state !== PENDING){
-        // 目前状态已经更改
-        return
-
+    if (this._state !== PENDING) {
+      // 目前状态已经更改
+      return;
     }
     this._state = newState;
     this._value = value;
@@ -49,8 +98,7 @@ class MyPromise {
   }
   /**
    * @description: 标记当前任务完成
-   * @param {*} reason 任务失败的相关数据
-   * @return {*}
+   * @param {*} reason 任务失败的相关数据   * @return {*}
    */
   _reject(reason) {
     this._changeState(REJECTED, reason);
@@ -58,8 +106,19 @@ class MyPromise {
 }
 
 const pro = new MyPromise((resolve, reject) => {
-//   resolve(123);
-//   reject(123)
-  throw new Error(123)
+  setTimeout(() => {
+    resolve(1);
+  }, 1000);
+ 
+  // throw new Error(123);
 });
+pro.then(
+  function A1() {},
+  // function A2() {}
+);
+pro.then(
+  function B1() {},
+  function B2() {}
+);
+
 console.log(pro);
